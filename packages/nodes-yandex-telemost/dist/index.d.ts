4943f1b9e5e658f45192b645dea6ff2f775c55a8
Firstname Lastname <ja200714@gmail.com>
2025-09-04 14:05:17 +0300

    chore: move package to repo root

--- packages/nodes-yandex-telemost/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { YandexOAuth2Api } from './credentials/YandexOAuth2Api.credentials';
-import { YandexTelemost } from './nodes/YandexTelemost/YandexTelemost.node';
-export declare const credentials: (typeof YandexOAuth2Api)[];
-export declare const nodes: (typeof YandexTelemost)[];
